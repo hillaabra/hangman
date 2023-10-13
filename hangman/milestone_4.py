014dc433e52d7f9b82449ea53f0d07b84c469c4a
Hilla <hilla.abramov@gmail.com>
2023-10-13 19:04:18 +0100

    add functionality to check_guess method

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -8,18 +8,22 @@ class Hangman():
     # a list of the letters of the word, with _ for each letter not yet guessed
     self.word_guessed = ['_' for char in self.word]
     # the number of unique letters in the word that have not been guessed yet
-    self.num_letters = len(set(self.word).difference(set(self.word_guessed)))
+    self.num_letters = len(set(self.word))
     # the number of lives the player had at the start of the game
-    self.num_ives = num_lives
+    self.num_lives = num_lives
     # a list of words
     self.word_list = word_list
     # a list of guesses, initially set to an empty list
     self.list_of_guesses = []
 
   def check_guess(self, guess):
-    guess.lower()
+    guess = guess.lower()
     if guess in self.word:
       print(f"Good guess! {guess} is in the word.")
+      for index in self.word:
+        if self.word[index] == guess:
+         self.word_guessed[index] = guess
+      self.num_letters -= 1
 
   def ask_for_input(self):
     while True:
