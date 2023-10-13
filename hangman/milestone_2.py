da19a4e486827d264275efd03838b7ee91732537
Hilla <hilla.abramov@gmail.com>
2023-10-13 17:28:19 +0100

    refactor milestone_2

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -4,17 +4,16 @@ import random
 word_list = ["lychee", "passionfruit", "cherry", "flat peach", "nectarine"]
 
 # Randomly generate a word from the word list using the random.choice method
-word = random.choice(word_list)
-
-# print that word to standard output
-print(word)
+word_to_guess = random.choice(word_list)
 
 # Ask the user for an input and assign it to a variable called `guess`
-guess = input("Enter a single letter: ")
+def get_user_input():
+  letter_guess = input("Enter a single letter: ")
+  # check the input is a single alphabetical character
+  if len(letter_guess) == 1 and letter_guess.isalpha():
+    print("Good guess!")
+  else:
+    print("Oops! That is not a valid input.")
+    get_user_input()
 
-# Chcek that the input is a single character
-if len(guess) == 1 and guess.isalpha():
-  print("Good guess!")
-else:
-  print("Oops! That is not a valid input.")
-
+get_user_input()
