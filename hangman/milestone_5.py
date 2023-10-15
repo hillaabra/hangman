cc1563f5354d7226c4aeb1f8b49c052e6504a47e
Hilla <hilla.abramov@gmail.com>
2023-10-15 20:25:03 +0100

    edit docstrings and correct print messages

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -2,17 +2,29 @@ import random
 
 class Hangman():
   '''
-  This class is used to represent a game of Hangman.
+  A Hangman Game that asks the user for a letter and checks if it is in the word.
+  It starts with a default number of lives and a random word from the word_list.
+
+  Parameters:
+  ----------
+  word_list: list
+      List of words to be chosen from for the game
+  num_lives: int
+      Number of lives, with a default value of 5 at
+      initialisation
 
   Attributes:
-      num_lives (int): initialised with 5 lives, the player
-          loses a life with each wrong letter guessed
-      word_guessed (list): a list representing the letters
-          of the word to be guessed, with '_' for each letter
-          of the word not yet guessed by the user
-      _ls_guessed_letters (list): protected; list keeping
-          record of users letter guesses, initialised as an
-          empty list
+  ----------
+  num_lives: int
+      Initialised with 5 lives, the player loses
+      a life with each wrong letter guessed
+  word_guessed: list
+      A list representing the letters of the word
+      to be guessed, with '_' for each letter of
+      the word not yet guessed by the player
+  _ls_guessed_letters: list
+      Protected; a list keeping record the player's
+      letter guesses, initialised as an empty list
   '''
   def __init__(self, word_list, num_lives=5):
     '''
@@ -26,8 +38,6 @@ class Hangman():
     self.__num_letters_left_to_guess = len(set(self.__word_to_guess))
     # the number of lives the player has at the start of the game, or left; public
     self.num_lives = num_lives
-    # a list of words to choose game word from; private
-    self.__word_list = word_list
     # letters guessed by the user so far, initially set to an empty list; protected
     self._ls_guessed_letters = []
 
@@ -37,8 +47,10 @@ class Hangman():
     word_guessed, the list representing the letters of the
     word guessed and not yet guessed by the player.
 
-    Args:
-        guessed_letter (str): the letter guessed by the player
+    Arguments:
+    ---------
+    guessed_letter: str
+        The letter guessed by the player
     '''
     for index in range(len(self.__word_to_guess)):
         if self.__word_to_guess[index] == guessed_letter:
@@ -67,8 +79,10 @@ class Hangman():
     update _ls_guessed_letters with the valid guesses
     of the player during the game.
 
-    Args:
-        guessed_letter (str): the letter guessed by the user.
+    Arguments:
+    ---------
+    guessed_letter: str
+        The letter guessed by the user
     '''
     self._ls_guessed_letters.append(guessed_letter)
 
@@ -78,8 +92,10 @@ class Hangman():
     check if the letter guessed by the user is in
     the word to be guessed.
 
-    Args:
-        guessed_letter (str): the letter guessed by the user.
+    Arguments:
+    ---------
+    guessed_letter: str
+        The letter guessed by the user
     '''
     if guessed_letter in self.__word_to_guess:
       print(f"Good guess! {guessed_letter} is in the word.")
@@ -95,11 +111,14 @@ class Hangman():
     This function is used internally to check if
     the user input is a single alphabetical character.
 
-    Args:
-        guessed_letter (str): the letter guessed by the user.
+    Arguments:
+    ---------
+    guessed_letter: str
+        The letter guessed by the user
 
     Returns:
-        bool: True if the input is valid, False otherwise.
+    -------
+    bool: True if the input is valid, False otherwise.
     '''
     return len(guessed_letter) == 1 and guessed_letter.isalpha() == True
 
@@ -128,12 +147,12 @@ class Hangman():
   def play_game(self):
     while True:
       if self.num_lives == 0:
-        print("You lost!")
+        print(f"You lost! The word was {self.__word_to_guess}")
         break
       elif self.__num_letters_left_to_guess > 0:
         self.ask_for_input()
       else:
-        print("Congratulations. You won the game!")
+        print("Congratulations! You won!")
         break
 
 def initialise_game(word_list):
