3490f7b62d824f510385d24a14131c9d6f965d81
Hilla <hilla.abramov@gmail.com>
2024-01-03 23:14:18 +0000

    formatting

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ import random
 from word_list import word_list
 
 
-class Hangman():
+class Hangman:
   '''
   A Hangman Game that asks the user for a letter and checks if it is in the word.
   It starts with a default number of lives and a random word from the word_list.
